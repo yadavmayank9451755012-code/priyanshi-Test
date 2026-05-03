1a3c0e87b5306e1ec826c8163c3ad4d15e69877c
yadavmayank9451755012-code <yadavmayank9451755012@gmail.com>
2026-05-04 01:04:16 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,24 @@
 import "./globals.css";
+import FloatingHearts from "./components/FloatingHearts"; // 👈 Import karo
 
 export const metadata = {
-  title: "Happy Birthday!",
-  description: "A special surprise..."
+  title: "Happy Birthday Madam Jii!",
+  description: "A special surprise...",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="antialiased">
-        {/* MusicPlayer capsule removed completely */}
-        {children}
+      {/* Background ko black hi rakhna taaki neon chamke */}
+      <body className="antialiased bg-black relative">
+        
+        {/* 🌟 Floating Hearts Background Component 🌟 */}
+        <FloatingHearts />
+        
+        {/* Baaki saare pages content (Children) */}
+        <div className="relative z-10">
+            {children}
+        </div>
       </body>
     </html>
   );
