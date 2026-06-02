0fead6b369eabd1b25da42ce6695a19da027c9e4
MAYANK <140157357+MayanKiz@users.noreply.github.com>
2026-06-02 15:49:58 +0530

    Update questions.js

--- src/app/data/questions.js
+++ src/app/data/questions.js
@@ -1,11 +1,11 @@
 export const QUESTIONS = [
- 
-    q: "How are youuu?",
+  {
+    q: "How are you?",
     options: [
-      { text: "mastt", reply: "Mast tum reho, noiceee badhiyaaa🌍" },
-      { text: "good goodd", reply: "Good ek word hai, main toh tumhrii dosti ke nashe mein hu 24/7. 🚀🤪" },
-      { text: "Pata nahi, bas jinda huu", reply: "Jinda rehna kaafi nahi. Main jaha hu, waha blast hona zaroori hai. kya bolti hooo! 💣😂" },
-      { text: "sad🥺", reply: "Sad? Teri ek aankh nam hui nahi ki main samandar le aau dhoone ke liye. Rone do ab mujhe bhi tumhri tension mein! 🌊🥲" }
+      { text: "Good", reply: "That's great!" },
+      { text: "Fine", reply: "Nice to hear that." },
+      { text: "Okay", reply: "Hope your day gets even better." },
+      { text: "Sad", reply: "Take care, things will improve." }
     ],
     gif: "/images/10.gif"
   }
