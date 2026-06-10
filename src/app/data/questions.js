ea468a7d7b96c6bc3bf971ec252fa74320e18a78
MAYANK <140157357+MayanKiz@users.noreply.github.com>
2026-06-10 20:07:48 +0530

    Update questions.js

--- src/app/data/questions.js
+++ src/app/data/questions.js
@@ -2,7 +2,7 @@ export const QUESTIONS = [
   {
     q: "How are youuu?",
     options: [
-      { text: "masttr", reply: "Good to hear! 😊" },
+      { text: "mastt", reply: "Good to hear! 😊" },
       { text: "good goodd", reply: "That's nice! ✨" },
       { text: "Pata nahi, bas jinda huu", reply: "Haha, take care! 😄" },
       { text: "sad🥺", reply: "Hope you feel better soon. 🫂" }
